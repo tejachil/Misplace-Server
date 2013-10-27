a7f972cc287ae47e3e1e761d5e7defdd07aba88e
Teja Chiluvuri <tejachil@vt.edu>
2013-10-27 06:18:55 -0400

    Login implemented to the /user servlet. Fixed some bugs.

--- Misplace-Server/src/edu/vt/teja/ece4564/misplaceserver/Global.java
+++ Misplace-Server/src/edu/vt/teja/ece4564/misplaceserver/Global.java
@@ -2,17 +2,18 @@ package edu.vt.teja.ece4564.misplaceserver;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-import com.google.gwt.dev.util.collect.HashMap;
 
 
 public class Global {
 	public static List<User> USERS_ = new ArrayList<User>();
 	public static HashMap<User, List<TagMessage.Tag>> TAGS_= new HashMap<User, List<TagMessage.Tag>>();
 	
-	public static int getIndexOfUser(String devid){
+	public static int getIndexOfUser(String username){
 		for(int i = 0; i < Global.USERS_.size(); ++i){
-			if(USERS_.get(i).deviceid == devid);	return i;
+			if(USERS_.get(i).username.equals(username))
+				return i;
 		}
 		return -1;
 	}
--- Misplace-Server/src/edu/vt/teja/ece4564/misplaceserver/server/UserService.java
+++ Misplace-Server/src/edu/vt/teja/ece4564/misplaceserver/server/UserService.java
@@ -34,12 +34,23 @@ public class UserService extends HttpServlet {
 			res.getWriter().println("Added: " + newUser.toString());
 		}
 		else if (req.getParameter("action").equals("edit")){
-			int index = Global.getIndexOfUser(req.getParameter("devid"));
-			Global.USERS_.get(index).firstname = req.getParameter("firstname");
-			Global.USERS_.get(index).lastname = req.getParameter("lastname");
-			Global.USERS_.get(index).username = req.getParameter("username");
-			Global.USERS_.get(index).password = req.getParameter("password");
-			res.getWriter().println("Updated: " + Global.USERS_.get(index).toString());			
+			int index = Global.getIndexOfUser(req.getParameter("username"));
+			if(index != -1){
+				Global.USERS_.get(index).firstname = req.getParameter("firstname");
+				Global.USERS_.get(index).lastname = req.getParameter("lastname");
+				Global.USERS_.get(index).deviceid = req.getParameter("devid");
+				Global.USERS_.get(index).password = req.getParameter("password");
+				res.getWriter().println("Updated: " + Global.USERS_.get(index).toString());
+			}
+			else res.getWriter().println("User does not exist. Please add.");
+		}
+		else if (req.getParameter("action").equals("login")){
+			int index = Global.getIndexOfUser(req.getParameter("username"));
+			if(index != -1 && 
+					Global.USERS_.get(index).username.equals(req.getParameter("username")) &&
+					Global.USERS_.get(index).password.equals(req.getParameter("password")))
+				res.getWriter().println("LOGIN SUCCESS!");
+			else	res.getWriter().println("LOGIN FAILED!");
 		}
 		else{
 			res.getWriter().println("USERS: ");
